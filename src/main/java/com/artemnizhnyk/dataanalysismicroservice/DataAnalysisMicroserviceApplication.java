439d927d31c65a51d107b961bac107915b5a69e7
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-01-26 22:39:57 +0100

    Initial commit

---
+++ src/main/java/com/artemnizhnyk/dataanalysismicroservice/DataAnalysisMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.dataanalysismicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalysisMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAnalysisMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/artemnizhnyk/dataanalysismicroservice/config/BeanConfig.java
@@ -0,0 +1,2 @@
+package com.artemnizhnyk.dataanalysismicroservice.config;public class BeanConfig {
+}
